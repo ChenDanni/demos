a80f6eeded4ce1aedefa628b1d71bc5aefc45eb9
dannichen <dannichen96@163.com>
2018-03-21 23:20:44 +0800

    https://doc.webpack-china.org/guides/asset-management    (css and image)

--- webpack-demo/src/js/index.js
+++ webpack-demo/src/js/index.js
@@ -2,11 +2,18 @@
  * Created by cdn on 2018/3/21.
  */
 import _ from 'lodash';
+import '../css/style.css';
+import Img from '../img/img1.jpg'
 
 function component() {
     var element = document.createElement('div');
 
+    var imgg = new Image();
+    imgg.src = Img;
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    element.appendChild(imgg);
 
     return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -4,12 +4,30 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
         //相对路径转化为绝对路径(from,to) 它可以接受多个参数，依次表示所要进入的路径，
         // 直到将最后一个参数转为绝对路径。如果根据参数无法得到绝对路径，就以当前所在路径作为基准。
         // 除了根目录，该方法的返回值都不带尾部的斜杠。
-        path: path.resolve(__dirname, 'dist/js')
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+
+            }
+        ]
     }
 };
